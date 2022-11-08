50a90719bf7c4d6697481cda6c08cc7a227ac47b
cseshohag <solaiman.shohag95@gmail.com>
2022-11-08 21:42:34 +0600

    Added a web api .net core project with models and controller

---
+++ RegistrationAPI/RegistrationAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace RegistrationAPI.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Gender { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
